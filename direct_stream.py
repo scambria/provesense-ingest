3fffd33f297c2b57546faaa0b5efaf4e6194e237
scambria <steve.cambria@istresearch.com>
2016-10-24 20:21:50 -0400

    Removed print statements + added in code to wait for kafka availability

--- direct_stream.py
+++ direct_stream.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
 
-import sys
 import os 
 import requests
+import sys
+import time 
 import uuid
 
 from pyspark import SparkContext
@@ -10,30 +11,15 @@ from pyspark.streaming import StreamingContext
 from pyspark.streaming.kafka import KafkaUtils
 
 def sendPartition(iter):
-    print('w**********SENDPARTITION**********1')    
-    headers = {'Content-Type': 'application/x-turtle '}
-    print('w**********SENDPARTITION**********2')
-    insert = '@prefix dc: <http://purl.org/dc/elements/1.1/> . dc:thing dc:test_errr2' + ' dc:test1 .'
-    print('w**********SENDPARTITION**********3')    
-    results = requests.post(os.getenv('SPARQL_ENDPOINT'), insert, headers=headers)    
-    print('w**********SENDPARTITION**********4')
-    i = 0
     for record in iter:
-        print('w**********SENDPARTITION**********5')        
         try:
             record = record.rstrip('\n')                        
-            print('w**********SENDPARTITION**********6')            
             headers = {'Content-Type': 'application/x-turtle '}
-#            insert = '@prefix dc: <http://purl.org/dc/elements/1.1/> . dc:thing dc:test_' + str(i) + ' dc:test' + str(i) + ' .'
-            print('w*********SENDING*********r' + str(record))
-            print('w*********SENDING' + str(record))
-            print(record)
-            print('w*********SENDING2*********r2' + str(record))
+            print('**********SENDING*********\n' + str(record))
             results = requests.post(os.getenv('SPARQL_ENDPOINT'), record, headers=headers)
-            print('w**********SENT2BLAZE**********7 ' + str(results))
-            print('w**********SEND2BLAZE**********text ' + str(results.text))
+            print('**********SENT2BLAZE**********\n ' + str(results) + '\n******************')
         except Exception as e:
-            print('w********BLAZEERROR*****************' + str(e))
+            print('**********BLAZEERROR*****************' + str(e))
             pass
 
 if __name__ == "__main__":
@@ -45,13 +31,16 @@ if __name__ == "__main__":
     
         sc = SparkContext(appName="ProvesenseDirectStream")
         ssc = StreamingContext(sc, 2)
-
         brokers, topic = sys.argv[1:]
-        kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
-        print('******CREATED KVS********')
-        headers = {'Content-Type': 'application/x-turtle '}
-        insert = '@prefix dc: <http://purl.org/dc/elements/1.1/> . dc:thing dc:test_errr1' + ' dc:test1 .'
-        results = requests.post(os.getenv('SPARQL_ENDPOINT'), insert, headers=headers)
+
+        while True:
+            try: 
+                kvs = KafkaUtils.createDirectStream(ssc, [topic], {"metadata.broker.list": brokers})
+                break
+            except:
+                print('Waiting on kafka...')
+                time.sleep(1)
+                pass
 
     except Exception as e:
         print('******************MAIN ERROR!***********' + str(e))
@@ -59,11 +48,3 @@ if __name__ == "__main__":
     kvs.map(lambda x: x[1]).foreachRDD(lambda rdd: rdd.foreachPartition(sendPartition))
     ssc.start()
     ssc.awaitTermination()
-
-# @prefix dc: <http://purl.org/dc/elements/1.1/> . dc:thing dc:testspark5 dc:test1 .        
-# >>> insert = '@prefix dc: <http://purl.org/dc/elements/1.1/> . dc:thing dc:test_py2 dc:test1 .' 
-# >>> results = requests.post('http://blazegraph:9999/blazegraph/sparql', insert, headers=headers)
-# >>> print(results.text)                                                                         
-# <?xml version="1.0"?><data modified="1" milliseconds="48"/>
-#curl -H 'Content-Type: text/turtle' -X POST -d 'PREFIX dc: <http://nonsense.org> INSERT {  dc:thing dc:test_py dc:test } WHERE {}' http://localhost:9090/blazegraph/sparql
-
